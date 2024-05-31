8443c4c9f614bc427bb06c32acb0af7bf85a7266
matteocenci <matteocenci01@gmail.com>
2024-05-31 16:18:13 +0200

    feat: create class Actor

---
+++ classes/Actor.php
@@ -0,0 +1,21 @@
+<?php
+
+class Actor {
+    private string $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+
+    }
+
+    public function getName(): string {
+        return $this->name;
+    }
+
+    public function setName(string $name): void {
+        $this->name = $name;
+    }
+    
+}
+
+?>
